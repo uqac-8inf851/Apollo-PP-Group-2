60151cfd3b4ec54a7a0eb70134ff43d0ee6abac5
betopluga <carlos@plugaweb.com.br>
2020-10-25 02:06:20 -0400

    app running on docker-compose

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,13 +12,9 @@
 </template>
 
 <script>
-import NavBar from '@/components/NavBar.vue'
-import Footer from '@/components/Footer.vue'
 
 export default {
   components: {
-    NavBar,
-    Footer
   }
 }
 
