c622c0abfb3794d8cfc53b6aa0aaa3b76756fcc7
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2020-11-14 21:02:09 -0500

    Added ECS log enconder for LogBack.

---
+++ backend/src/main/java/com/apollo/backend/controller/LoggingController.java
@@ -0,0 +1,26 @@
+package com.apollo.backend.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class LoggingController {
+ 
+    Logger logger = LoggerFactory.getLogger(LoggingController.class);
+ 
+    @RequestMapping(method = RequestMethod.GET, value = "/logs")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+ 
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
