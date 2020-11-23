171953dbf9ac28153d9ef9ecd3570f64ec0289a4
Mário Silva <guri.vds@bol.com.br>
2020-11-23 12:11:26 -0300

    add tooltip in btn menu

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -8,21 +8,68 @@
     <v-spacer></v-spacer>
     <v-divider class="mx-3 my-5"></v-divider>
 
-    <v-btn icon x-large to="/">
-      <v-icon dark>mdi-billboard</v-icon>
-    </v-btn>
-    <v-btn icon x-large to="/project">
-      <v-icon dark>mdi-clipboard-text-outline</v-icon>
-    </v-btn>
-    <v-btn icon x-large>
-      <v-icon dark>mdi-file-chart-outline</v-icon>
-    </v-btn>
-    <v-btn icon x-large>
-      <v-icon dark>mdi-account</v-icon>
-    </v-btn>
-    <v-btn icon x-large>
-      <v-icon dark>mdi-settings-outline</v-icon>
-    </v-btn>
+    <v-tooltip right>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          icon x-large
+          v-bind="attrs"
+          v-on="on"
+          to="/"
+        >
+          <v-icon dark>mdi-billboard</v-icon>
+        </v-btn>
+      </template>
+      <span>Program</span>
+    </v-tooltip>
+    <v-tooltip right>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          icon x-large
+          v-bind="attrs"
+          v-on="on"
+          to="/project"
+        >
+          <v-icon dark>mdi-clipboard-text-outline</v-icon>
+        </v-btn>
+      </template>
+      <span>Project</span>
+    </v-tooltip>
+    <v-tooltip right>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          icon x-large
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon dark>mdi-file-chart-outline</v-icon>
+        </v-btn>
+      </template>
+      <span>Report</span>
+    </v-tooltip>
+    <v-tooltip right>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          icon x-large
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon dark>mdi-account</v-icon>
+        </v-btn>
+      </template>
+      <span>User</span>
+    </v-tooltip>
+    <v-tooltip right>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          icon x-large
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon dark>mdi-settings-outline</v-icon>
+        </v-btn>
+      </template>
+      <span>Settings</span>
+    </v-tooltip>
   </v-navigation-drawer>
 </template>
 
